a18fc7099acc061dd080570692a01fcc867dceb5
Igor Androsov <iandrosov@salesforce.com>
2021-11-01 08:45:34 +0900

    Remove DB URL

--- config/default.js
+++ config/default.js
@@ -30,8 +30,8 @@ module.exports = {
   /**
    * Postresql database connection string.
    */
-  DATABASE_URL: process.env.DATABASE_URL || 'postgres://rxsvucbfbmgfiw:180fa5453ccf3ef58140a894efa46ab660ad929484c58d158605c3a4c7cd9470@ec2-107-23-143-66.compute-1.amazonaws.com:5432/dfu3nb57chc8mc',
-  // DF16tdx17 DATABASE_URL: process.env.DATABASE_URL || 'postgres://zfjhwolposglpi:eeO819Q6hNfvDLZBh6nfSoMdfH@ec2-54-83-47-145.compute-1.amazonaws.com:5432/deu9n2ng667tus?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory',
+  DATABASE_URL: process.env.DATABASE_URL || 'URL',
+  
   /**
    * String returned in "status" field of successfull responses.
    */
